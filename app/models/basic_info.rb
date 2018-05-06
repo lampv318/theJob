f8d5f441c22d5862c07bfb8cac81aeacd4527923
lampv <lampvhy@gmail.com>
2018-05-06 17:43:08 +0700

    autocomple in search

--- app/models/basic_info.rb
+++ app/models/basic_info.rb
@@ -5,7 +5,5 @@ class BasicInfo < ApplicationRecord
   scope :salary_gt, ->(amount) { where('salary > ?', amount)  }
   scope :salary_lt, ->(amount) { where('salary < ?', amount)  }
 
-  validates :user_avatar, presence: :true
-
   mount_uploader :user_avatar, AvatarUploader
 end
